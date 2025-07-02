28d98ad1f90c995c7ca16189673e7aa8f4f108a0
marcusdevsurfer <marcus.sea13@gmail.com>
2025-07-02 13:31:13 -0600

    initial commit

---
+++ src/main/java/com/ai/eva_01/Eva01Application.java
@@ -0,0 +1,13 @@
+package com.ai.eva_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eva01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eva01Application.class, args);
+	}
+
+}
